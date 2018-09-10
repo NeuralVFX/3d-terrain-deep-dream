249b5f78e7761df35912a4700a4337fe846f092b
NeuralVFX <vfxhoneybadger@gmail.com>
2018-09-10 23:02:47 +0700

    making some shell scripts to pull data and dem file

--- dem/get_test_dem.sh
+++ dem/get_test_dem.sh
@@ -4,5 +4,4 @@ URL=https://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/1m/IMG/USGS_NED_on
 ZIP_FILE=./dem/x34y441_CO.zip
 wget $URL -O $ZIP_FILE
 unzip $ZIP_FILE -d ./dem/
-
-
+mv ./dem/USGS_NED_one_meter_x34y441_CO_Central_Western_2016_IMG_2018.img ./dem/x34y441_CO.img
